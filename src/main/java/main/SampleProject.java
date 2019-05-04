f0fa8b0aba0121b16b45356e9ca79514f37d0f2d
kulkarni <kulkarni@Paresh>
2019-05-04 13:03:00 +0530

    written add and printHello function successfully

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -1,17 +1,20 @@
 package main;
 
-public class SampleProject {
-	//print  Hello world! in the console
-	public static String printHello() {
-		//print Hello world! in the console!
+public class SampleProject
+{
+	
+	public static String printHello() 
+	{
 		String c = "Hello World!";
 		return c;
 	}
-	public static int add(int a,int b) {
+	public static int add(int a,int b)
+	{
 		int c = a+b;
 		return c;
 	}
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
 		System.out.println(printHello());
 		System.out.println(add(23,45));
 	}
